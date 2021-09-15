3e3b6e94f53419be6f5f4572c4624d0f8047aebb
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-14 21:17:25 -0400

    4 tab space format

--- markov.js
+++ markov.js
@@ -1,49 +1,52 @@
 /** Textual markov chain generator */
 
 class MarkovMachine {
-  /** build markov machine; read in text.*/
+    /** build markov machine; read in text.*/
 
-  constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter((c) => c !== "");
-    this.obj = {};
-    this.makeChains();
-  }
-
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
-
-  makeChains() {
-    // TODO
-    let temp;
-    for (let i = 0; i < this.words.length; i++) {
-      temp = this.words[i + 1] === undefined ? null : this.words[i + 1];
-      if (this.obj.hasOwnProperty(this.words[i])) {
-        this.obj[this.words[i]].push(temp);
-      } else {
-        this.obj[this.words[i]] = [temp];
-      }
+    constructor(text) {
+        let words = text.split(/[ \r\n]+/);
+        this.words = words.filter((c) => c !== "");
+        this.obj = {};
+        this.makeChains();
     }
-  }
 
-  /** return random text from chains */
+    /** set markov chains:
+     *
+     *  for text of "the cat in the hat", chains will be
+     *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeText(numWords = 100) {
-    // TODO
-    let word = Object.keys(this.obj)[
-      Math.floor(Math.random() * Object.keys(this.obj).length)
-    ];
-    let sentence = word;
-    while (word !== null) {
-      word = this.obj[word][Math.floor(Math.random() * this.obj[word].length)];
-      if (word !== null) {
-        sentence += " " + word;
-      }
+    makeChains() {
+        // TODO
+        let temp;
+        for (let i = 0; i < this.words.length; i++) {
+            temp = this.words[i + 1] === undefined ? null : this.words[i + 1];
+            if (this.obj.hasOwnProperty(this.words[i])) {
+                this.obj[this.words[i]].push(temp);
+            } else {
+                this.obj[this.words[i]] = [temp];
+            }
+        }
     }
-    return sentence;
-  }
+
+    /** return random text from chains */
+
+    makeText(numWords = 100) {
+        // TODO
+        let word = Object.keys(this.obj)[
+            Math.floor(Math.random() * Object.keys(this.obj).length)
+        ];
+        let sentence = word;
+        while (word !== null) {
+            word =
+                this.obj[word][
+                    Math.floor(Math.random() * this.obj[word].length)
+                ];
+            if (word !== null) {
+                sentence += " " + word;
+            }
+        }
+        return sentence;
+    }
 }
 
 module.exports = { MarkovMachine };
